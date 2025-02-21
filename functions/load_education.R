7b8029b7278290534208f0da7ea1ccca393e262b
bpffjl <48254282+bpffjl@users.noreply.github.com>
2025-02-21 13:29:27 -0600

    Updated functions

--- functions/load_education.R
+++ functions/load_education.R
@@ -1,8 +1,10 @@
-load_education <- function()
+load_education <- function(google_sheet)
 {
   gs4_deauth()
   out <- read_sheet(
-    "https://docs.google.com/spreadsheets/d/1q_xOb6RizwdmZ0qgTWpW-PFmfX_qXJ65kpdDH5pnxP8/edit?gid=0#gid=0") %>%
+    # "https://docs.google.com/spreadsheets/d/1q_xOb6RizwdmZ0qgTWpW-PFmfX_qXJ65kpdDH5pnxP8/edit?gid=0#gid=0"
+    google_sheet
+    ) %>%
     arrange(desc(Year))
   return(out)
 }
--- functions/load_jobs.R
+++ functions/load_jobs.R
@@ -1,7 +1,9 @@
-load_jobs <- function()
+load_jobs <- function(google_sheet)
 {
   gs4_deauth()
   out <- read_sheet(
-    "https://docs.google.com/spreadsheets/d/1unv9PrAeBjS2wYR7IMPw2fjf_tqER5cROWqlvuzkun8/edit?gid=0#gid=0")
+    # "https://docs.google.com/spreadsheets/d/1unv9PrAeBjS2wYR7IMPw2fjf_tqER5cROWqlvuzkun8/edit?gid=0#gid=0"
+    google_sheet
+    )
   return(out)
 }
